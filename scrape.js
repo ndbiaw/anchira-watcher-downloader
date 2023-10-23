8072745c443b3eda545e8caafc099574ed94344e
Ling <59746573+ndbiaw@users.noreply.github.com>
2023-10-23 22:39:56 +0700

    chore: headless

--- scrape.js
+++ scrape.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: 'new' });
 
   const urls = fs.readFileSync('urls.txt', 'utf-8').split('\n');
 
